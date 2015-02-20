80c712f340eaa4319052c6939a81fec855702796
psytau <rmanninoFoo@gmail.com>
2015-02-20 18:31:54 +0800

    update test

--- test/controllers/play_controller_test.rb
+++ test/controllers/play_controller_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class PlayControllerTest < ActionController::TestCase
+  setup do
+    @conversation = conversations(:one)
+  end
+
   test "should get play" do
-    get :play
+    get :play, :id => 1
     assert_response :success
   end
 
